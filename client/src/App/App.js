8f3c4c8dd320bf6d32918dfd25bc950c06240dd8
AdhamHaisami <adham9633@gmail.com>
2021-03-27 21:14:46 +0300

    install material-ui/core package, customizing theme and wrapping the app component with themeProvider Relates #15

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,5 +1,18 @@
+import { ThemeProvider } from '@material-ui/core/styles';
+
+import { Button } from '@material-ui/core';
+import theme from './theme';
+
 const App = () => {
-  return <div>hi</div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div>
+        <Button variant="outlined" color="primary">
+          I'm a button
+        </Button>
+      </div>
+    </ThemeProvider>
+  );
 };
 
 export default App;
---
+++ client/src/App/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#CFAB7A',
+    },
+    secondary: {
+      main: '#FFFFFF',
+    },
+  },
+});
+
+export default theme;

2b91cd0f532f0c0b8113506cb7abf736e4bf0a44
AdhamHaisami <adham9633@gmail.com>
2021-03-29 00:07:09 +0300

    customize component in theme file, add new font

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,15 +1,22 @@
 import { ThemeProvider } from '@material-ui/core/styles';
 
-import { Button } from '@material-ui/core';
+import { Button, Card, TextField, Typography } from '@material-ui/core';
 import theme from './theme';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <div>
-        <Button variant="outlined" color="primary">
+     
+        <Card>
+        <Button color='primary' variant='contained' >
+          I'm a button
+        </Button><Button color='secondary' variant='contained' >
           I'm a button
         </Button>
+        <Typography> Hello wold</Typography>
+        </Card>
+        <TextField label="Hello"></TextField>
       </div>
     </ThemeProvider>
   );
--- client/src/App/theme.js
+++ client/src/App/theme.js
@@ -2,13 +2,52 @@ import { createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme({
   palette: {
+   type: 'dark',
     primary: {
       main: '#CFAB7A',
+      
     },
     secondary: {
       main: '#FFFFFF',
     },
   },
+  typography: {
+    fontFamily: "Syne Mono",
+    button: {
+      color: "#CFAB7A"
+    }
+  },
+  shape: {
+    borderRadius: '3px'
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        textTransform: 'none',      
+      },
+    },
+    MuiCard : {
+      root: {
+        backgroundColor: '#000000',
+      },
+      
+    }
+  },
+  props: {
+    MuiButton: {
+      disableRipple: 'true',
+      variant : "outlined",
+      color: "primary",
+    },
+    MuiTextField: {
+      InputLabelProps: {
+        shrink: true,
+      },
+      variant: 'filled',
+      
+    },
+   
+  }
 });
 
 export default theme;

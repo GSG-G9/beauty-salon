0322abce08f18991c0666e79615a2c3d7fd4ae09
yasmeenattallah <yattallah7@gmail.com>
2021-03-28 12:06:29 +0300

    react router dom setup Relates #22

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -1,5 +1,19 @@
-const App = () => {
-  return <div>hi</div>;
-};
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+ } from "react-router-dom";
+
+const App = ()=>{
+  return (
+    <Router>
+      <Switch>
+      <div>hi</div>
+      </Switch>
+    </Router>
+  )
+}
+
 
 export default App;

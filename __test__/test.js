d66543fedf71f36cc158356405e2e72865faa848
Khamis Hesham <khamis1997.kh@gmail.com>
2021-03-29 15:20:41 +0300

    made test for signup route

--- __test__/test.js
+++ __test__/test.js
@@ -1,5 +1,47 @@
-describe('dummy test', () => {
-  it('should Always return true', () => {
-    expect(true).toBe(true);
+// eslint-disable-next-line import/no-extraneous-dependencies
+const request = require('supertest');
+const app = require('../server/app');
+const { runBuild } = require('../server/database/config/build');
+const connection = require('../server/database/config/connection');
+
+describe('sign up routes tests', () => {
+  beforeEach(() => runBuild());
+  afterAll(() => connection.end());
+  const userData = {
+    firstName: 'khamis',
+    lastName: 'hesham',
+    email: 'khamis@gmail.com',
+    password: '123456',
+  };
+
+  test('check if email exist, status should be 409 and (user already exist) message', async () => {
+    const {
+      body: { message },
+      statusCode,
+    } = await request(app)
+      .post('/api/v1/signup')
+      .set({
+        'Content-Type': 'application/json',
+      })
+      .send(JSON.stringify({ ...userData, email: 'yatt@gmail.com' }));
+    expect(message).toBe('user already exists ');
+    expect(statusCode).toBe(409);
+  });
+
+  test('check successful sign-up, status should be 200 and (signed up successfully) message', async () => {
+    const {
+      body: { message },
+      statusCode,
+    } = await request(app)
+      .post('/api/v1/signup')
+      .set({
+        'Content-Type': 'application/json',
+      })
+      .send({
+        ...userData,
+        email: 'khamis@gmail.com',
+      });
+    expect(statusCode).toBe(201);
+    expect(message).toBe('SingUp Successfully');
   });
 });

0e77c00b33ae9916012eca584ff3902d73d90405
yasmeenattallah <yattallah7@gmail.com>
2021-03-30 12:26:00 +0300

    test signin Route in a new file in folder test Relates #20

---
+++ __test__/signin.test.js
@@ -0,0 +1,55 @@
+const request = require('supertest');
+
+const app = require('../server/app');
+const { runBuild } = require('../server/database/config/build');
+const connection = require('../server/database/config/connection');
+
+describe('POST /api/v1/login', () => {
+  beforeEach(() => runBuild());
+  afterAll(() => connection.end());
+  test('router returns 400 if there is no password', async () => {
+    const { statusCode } = await request(app)
+      .post('/api/v1/signin')
+      .send({ email: 'yatt@gmail.com', password: '' });
+    return expect(statusCode).toBe(400);
+  });
+
+  test('router returns 400 if there is no email', async () => {
+    const { statusCode } = await request(app)
+      .post('/api/v1/signin')
+      .send({ email: '', password: '123456789yas' });
+    return expect(statusCode).toBe(400);
+  });
+
+  test('router returns 200 if user logged in successfully', async () => {
+    const {
+      statusCode,
+    } = await request(app).post('/api/v1/signin').send({
+      email: 'yatt@gmail.com',
+      password: '123456789yas',
+    });
+    return expect(statusCode).toBe(200);
+  });
+  test('router returns 400 if password is incorrect', async () => {
+    const {
+      body: { Message },
+      statusCode,
+    } = await request(app)
+      .post('/api/v1/signin')
+      .set({
+        'Content-Type': 'application/json',
+      })
+      .send(
+        JSON.stringify({ email: 'yatt@gmail.com', password: '1234567888yas' }),
+      );
+    expect(statusCode).toBe(400);
+    return expect(Message).toBe('Invalid username/password');
+  });
+
+  test('router returns 404 if email is incorrect', async () => {
+    const { statusCode } = await request(app)
+      .post('/api/v1/signin')
+      .send({ email: 'yatdt@gmail.com', password: '123456789yas' });
+    return expect(statusCode).toBe(404);
+  });
+});

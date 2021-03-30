0845bafa1595b47fe6a10de9f2281de4e8df1c7b
Khamis Hesham <khamis1997.kh@gmail.com>
2021-03-30 10:15:32 +0300

    fix signup validation file

--- server/utilis/validation/signupValidation.js
+++ server/utilis/validation/signupValidation.js
@@ -1,8 +1,8 @@
 const joi = require('joi');
 
 const signupValidation = joi.object({
-  firstName: joi.string().required(),
-  lastName: joi.string().required(),
+  firstName: joi.string().alphanum().min(3).required(),
+  lastName: joi.string().alphanum().min(3).required(),
   email: joi.string().email().required(),
   password: joi.string().min(6).required(),
 });

6efa98549a7b3d858cedccc7fb5945f11083a6b7
Khamis Hesham <khamis1997.kh@gmail.com>
2021-03-30 23:01:47 +0300

    added getBookingsByDate to the router

--- server/routes/common.js
+++ server/routes/common.js
@@ -1,7 +1,8 @@
 const commonRouter = require('express').Router();
 
-const { signUp } = require('../controller');
+const { signUp, getBookingsByDate } = require('../controller');
 
 commonRouter.post('/signup', signUp);
+commonRouter.get('/bookings/:date', getBookingsByDate);
 
 module.exports = commonRouter;

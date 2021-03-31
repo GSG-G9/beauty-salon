da43086ff759ee1c50a6a939a4035b090deaaeda
imansedky5 <imans.ewaiti@gmail.com>
2021-03-31 13:47:26 +0300

    removed unnecessary comments

--- server/controller/common/contactus.js
+++ server/controller/common/contactus.js
@@ -3,30 +3,10 @@ const { contactusValidation, boomify } = require('../../utilis');
 
 const contactusHandler = async (req, res, next) => {
   try {
-    const { name, email, message, mobile } = req.body;
-    // try {
-    const value = await contactusValidation.validateAsync({
-      name,
-      email,
-      message,
-      mobile,
-    });
-    // const { error, value } = contactusValidation.validate({
-    //   name,
-    //   email,
-    //   message,
-    //   mobile,
-    // });
-    // console.log(error, 5);
-    // if (error) {
-    //   next(boomify(400, error.details[0].message));
-    // }else{
-
-    // }
-
-    // } catch (err) {
-    //   throw boomify(400, err.details[0].message);
-    // }
+    const {
+      name, email, message, mobile,
+    } = req.body;
+    await contactusValidation.validateAsync(req.body);
 
     await postMessage(name, email, message, mobile);
 
@@ -38,7 +18,7 @@ const contactusHandler = async (req, res, next) => {
     next(
       err.name === 'ValidationError'
         ? boomify(400, err.details[0].message)
-        : err
+        : err,
     );
   }
 };

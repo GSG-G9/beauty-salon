34b8603cf63b4c8e01fede209530dcbef0bc3ad4
Khamis Hesham <khamis1997.kh@gmail.com>
2021-04-01 13:47:02 +0300

    add update profile to the router file

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,7 +1,9 @@
 const userRouter = require('express').Router();
 
 const { logOut } = require('../controller');
+const { updateUserController } = require('../controller');
 
 userRouter.post('/logout', logOut);
+userRouter.patch('/profile', updateUserController);
 
 module.exports = userRouter;

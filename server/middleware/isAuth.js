5ca42fec49d7376cd9dbbacaa642ca5408982ef2
mFlafiel <msf-2oo7@hotmail.com>
2021-04-01 11:38:03 +0300

    handle the case when token is missing relates #25

--- server/middleware/isAuth.js
+++ server/middleware/isAuth.js
@@ -6,14 +6,18 @@ const isAuth = async (req, res, next) => {
     const decodedUserData = await verifyToken(token);
     const { id, role } = decodedUserData;
     if (!decodedUserData) {
-      throw boomify(401, "you're unautherized");
+      throw boomify(401, "you're unauthorized");
     } else {
       req.userId = id;
       req.role = role;
       next();
     }
   } catch (err) {
+    if (err.message === 'jwt must be provided') {
+      next(boomify(401, "you're unauthorized"));
+    }
     next(err);
   }
 };
+
 module.exports = isAuth;

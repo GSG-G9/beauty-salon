9d245d2d906874fd640a5a4f3e18ad5faa7a351d
imansedky5 <imans.ewaiti@gmail.com>
2021-04-02 14:56:38 +0300

    created tests for isAuth route and transfered logout test into protectedRoutes.test.js

---
+++ __test__/protectedRoutes.test.js
@@ -0,0 +1,45 @@
+const request = require('supertest');
+const cookie = require('cookie');
+const app = require('../server/app');
+const { runBuild } = require('../server/database/config/build');
+const connection = require('../server/database/config/connection');
+
+describe('test getUserData query and /api/v1/isAuth route', () => {
+  beforeEach(() => runBuild());
+  afterAll(async () => { await new Promise((resolve) => setTimeout(() => resolve(), 3000)); });
+  afterAll(() => connection.end());
+
+  let token;
+
+  // eslint-disable-next-line jest/expect-expect
+  test('login user to utilize it\'s token in unprotected routes tests', async () => {
+    const { header: { 'set-cookie': cookies } } = await request(app)
+      .post('/api/v1/signin')
+      .send({ email: 'abood@gmail.com', password: 'abood123' });
+    token = cookie.parse(cookies[0]).token;
+  });
+
+  test('GET /api/v1/isAuth, should return all user data', async () => {
+    const { body: { data } } = await request(app)
+      .get('/api/v1/isAuth')
+      .set('Cookie', [`token=${token}`])
+      .expect('Content-Type', /json/);
+    expect(data).toEqual(expect.objectContaining({
+      id: 5,
+      first_name: 'abood',
+      email: 'abood@gmail.com',
+      role: 'user',
+    }));
+  });
+
+  test('should return successfully logout with status 200 with message logged out successfully', async () => {
+    const {
+      statusCode,
+      body: { message },
+    } = await request(app)
+      .post('/api/v1/logout')
+      .set('Cookie', [`token=${token}`]);
+    expect(statusCode).toBe(200);
+    expect(message).toBe('logged out successfully');
+  });
+});

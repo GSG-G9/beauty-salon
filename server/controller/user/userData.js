3375728e1fd3de00a14fdf4ad97e59715050ff53
imansedky5 <imans.ewaiti@gmail.com>
2021-04-03 12:00:28 +0300

    removed user password from user data

--- server/controller/user/userData.js
+++ server/controller/user/userData.js
@@ -3,6 +3,9 @@ const { getUserData } = require('../../database/queries');
 const userData = async (req, res, next) => {
   try {
     const { rows } = await getUserData(req.userId);
+
+    delete rows[0].password;
+
     res.json({
       statusCode: 200,
       data: rows[0],

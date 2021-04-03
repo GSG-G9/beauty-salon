8288336f2421a08b77e2e83ea79292d04fa8aa82
Khamis Hesham <khamis1997.kh@gmail.com>
2021-04-03 04:39:09 +0300

    add more information for the update

--- server/controller/user/updateUserController.js
+++ server/controller/user/updateUserController.js
@@ -5,23 +5,27 @@ const updateUserController = async (req, res, next) => {
   try {
     const { userId } = req;
     const {
-      firstName, lastName, mobile, address,
+      firstName, lastName, mobile, address, country, city, altMobile,
     } = await updateUserValidation.validateAsync(req.body, {
       abortEarly: false,
     });
 
     const { rows } = await updateUserQuery({
-      firstName, lastName, mobile, address, userId,
+      firstName, lastName, mobile, address, country, city, altMobile, userId,
     });
 
     if (!rows.length) {
       throw boomify(400, 'update failed');
     }
-
+    const {
+      id, first_name, last_name, email, alt_mobile, created_at,
+    } = rows[0];
     res.status(200).json({
       statusCode: 200,
       message: 'user updated Successfully',
-      data: rows,
+      data: {
+        id, first_name, last_name, email, country, city, address, mobile, alt_mobile, created_at,
+      },
     });
   } catch (err) {
     next(
--- server/database/queries/updateUserQuery.js
+++ server/database/queries/updateUserQuery.js
@@ -1,11 +1,11 @@
 const connection = require('../config/connection');
 
 const updateUserQuery = ({
-  firstName, lastName, mobile, address, userId,
+  firstName, lastName, mobile, address, country, city, altMobile, userId,
 }) => {
   const sql = {
-    text: 'UPDATE users SET first_name = $1,last_name = $2, mobile =$3, address = $4 WHERE id = $5  RETURNING *;',
-    values: [firstName, lastName, mobile, address, userId],
+    text: 'UPDATE users SET first_name = $1,last_name = $2, mobile =$3, address = $4, country = $5, city = $6, alt_mobile = $7 WHERE id = $8  RETURNING *;',
+    values: [firstName, lastName, mobile, address, country, city, altMobile, userId],
   };
   return connection.query(sql);
 };

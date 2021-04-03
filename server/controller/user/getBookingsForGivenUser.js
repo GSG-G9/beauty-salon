f4ea9d4051bfe9d87964ef0bbbd8e6e9aaec194c
imansedky5 <imans.ewaiti@gmail.com>
2021-04-03 20:05:48 +0300

    changed booking path and status code incase there's no appointments

--- server/controller/user/getBookingsForGivenUser.js
+++ server/controller/user/getBookingsForGivenUser.js
@@ -1,4 +1,3 @@
-const { boomify } = require('../../utilis/index');
 const { getBookingsByUserId } = require('../../database/queries');
 
 const getBookingsForGivenUser = async (req, res, next) => {
@@ -7,7 +6,10 @@ const getBookingsForGivenUser = async (req, res, next) => {
   const { rows, rowCount } = await getBookingsByUserId(userId);
 
   if (rowCount === 0) {
-    next(boomify(400, "you don't have appointments!"));
+    res.json({
+      statusCode: 200,
+      message: "you don't have appointments!",
+    });
   } else {
     res.json({
       statusCode: 200,
--- server/routes/user.js
+++ server/routes/user.js
@@ -3,6 +3,6 @@ const userRouter = require('express').Router();
 const { logOut, getBookingsForGivenUser } = require('../controller');
 
 userRouter.post('/logout', logOut);
-userRouter.get('/bookings', getBookingsForGivenUser);
+userRouter.get('/booking', getBookingsForGivenUser);
 
 module.exports = userRouter;

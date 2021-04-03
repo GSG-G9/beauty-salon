d839578b562ee1ec3c69004e096324bd39cbe264
AdhamHaisami <adham9633@gmail.com>
2021-04-03 04:28:41 +0300

    Add one test for success case, Corrected some misspellings Fix #32

--- __test__/getProfile.test.js
+++ __test__/getProfile.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
+
 const app = require('../server/app');
 const { runBuild } = require('../server/database/config/build');
 const connection = require('../server/database/config/connection');
 const { getProfile } = require('../server/database/queries');
 
 describe('Test getUser DB query and route', () => {
+  let token;
   beforeEach(() => runBuild());
   afterAll(() => connection.end());
   afterAll(async () => {
@@ -20,6 +22,20 @@ describe('Test getUser DB query and route', () => {
       }),
     );
   });
+  test('Login', async () => {
+    const res = await request(app)
+      .post('/api/v1/signin')
+      .send({
+        email: 'adham@hi.com',
+        password: '123456789',
+      });
+    const { headers: { 'set-cookie': [resCookies] } } = res;
+    token = resCookies.split(';')[0].split('=')[1];
+    return expect(res.body).toEqual({
+      statusCode: 200,
+      message: 'Login successfully',
+    });
+  });
   test('should return Object contains status code and data', async () => {
     const res = await request(app)
       .get('/api/v1/profile')
@@ -27,8 +43,27 @@ describe('Test getUser DB query and route', () => {
 
     const actual = res.body;
     const expected = { statusCode: 401, message: "you're unauthorized" };
-    expect(actual).toEqual(
-      expect.objectContaining(expected),
-    );
+    expect(actual).toEqual(expected);
+  });
+  test('Success Case', async () => {
+    const res = await request(app)
+      .get('/api/v1/profile')
+      .set('Cookie', [`token=${token}`])
+      .expect('Content-Type', /json/);
+    const actual = res.body;
+    const expected = {
+      status: 200,
+      data: {
+        first_name: 'adham',
+        last_name: 'Hai',
+        email: 'adham@hi.com',
+        country: null,
+        city: null,
+        address: null,
+        mobile: null,
+        alt_mobile: null,
+      },
+    };
+    expect(actual).toEqual(expected);
   });
 });
--- server/controller/user/getProfile.js
+++ server/controller/user/getProfile.js
@@ -5,13 +5,12 @@ const getUserProfile = async (req, res, next) => {
   try {
     const { userId } = req;
     const { rows } = await getProfile(userId);
-    console.log(rows);
     if (rows.length === 0) {
-      throw boomify(404, 'User doesn not exist');
+      throw boomify(404, 'User does not exist');
     }
     res.json({
       status: 200,
-      data: rows,
+      data: rows[0],
     });
   } catch (error) {
     next(error);

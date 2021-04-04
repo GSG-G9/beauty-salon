238c26454ec4c679ae5ab7092038587042f440e8
Khamis Hesham <khamis1997.kh@gmail.com>
2021-04-04 14:24:58 +0300

    make default props for variant and color

--- client/src/component/Button/index.js
+++ client/src/component/Button/index.js
@@ -3,18 +3,20 @@ import { PropTypes } from 'prop-types';
 import { Button } from '@material-ui/core';
 
 const ButtonComponent = ({ variant, color, children, ...rest }) => (
-  <>
-    <Button variant={variant} color={color} {...rest}>
-      {children}
-    </Button>
-  </>
+  <Button variant={variant} color={color} {...rest}>
+    {children}
+  </Button>
 );
 const { string, node } = PropTypes;
 
 ButtonComponent.propTypes = {
-  variant: string.isRequired,
-  color: string.isRequired,
+  variant: string,
+  color: string,
   children: node.isRequired,
+};
+ButtonComponent.defaultProps = {
+  variant: 'contained',
+  color: 'primary',
 };
 
 export default ButtonComponent;

bbd75fc5bf41d0c9025838cfde263f0c8af171e0
yasmeenattallah <yattallah7@gmail.com>
2021-04-08 11:42:38 +0300

    signin page Fix #14

--- client/src/pages/Signin/index.js
+++ client/src/pages/Signin/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import Container from '@material-ui/core/Container';
+import { Typography } from '@material-ui/core';
+import { InputField, ButtonComponent } from '../../component';
+import useStyles from './style';
 
-const Signin = () => <div>Hi from Signin</div>;
+const Signin = () => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.root}>
+      <form className={classes.form}>
+        <InputField label="Email" />
+        <InputField label="password" />
+        <ButtonComponent> Login </ButtonComponent>
+        <Typography color="primary">
+          Don&#39;t have an account ? sign up
+        </Typography>
+      </form>
+    </Container>
+  );
+};
 
 export default Signin;

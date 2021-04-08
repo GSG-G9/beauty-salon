f251b33e12bc1068cc5090894634311098b9a254
AdhamHaisami <adham9633@gmail.com>
2021-04-08 17:25:14 +0300

    Start building the Home Page, and it's style file Fix #2

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,5 +1,55 @@
 import React from 'react';
 
-const Home = () => <div>Hi from Home</div>;
+import Typography from '@material-ui/core/Typography';
+import { Button } from '@material-ui/core';
+import { ButtonComponent, ServiceCardForHomePage } from '../../component';
+
+import useStyles from './style';
+
+const Home = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.firstSec}>
+        <div className={classes.intro}>
+          <Typography color="primary" variant="h4" className={classes.text}>
+            Endless looks. We are committed to creating the perfect style for
+            each and every one of our clients.
+          </Typography>
+          <Typography variant="h6" className={classes.text2}>
+            Best brands of products. we will deliver products to your house,
+            Shopping is much easier now!!
+          </Typography>
+          <ButtonComponent
+            variant="outlined"
+            color="primary"
+            className={classes.button}
+          >
+            BookNow
+          </ButtonComponent>
+          <Button
+            variant="outlined"
+            color="contained"
+            className={`${classes.button} ${classes.buyNowBtn}`}
+          >
+            Buy Product
+          </Button>
+        </div>
+      </div>
+      <div className={classes.servicesSec}>
+        <Typography variant="h4" color="primary">
+          Our Services
+        </Typography>
+        <div className={classes.services}>
+          <ServiceCardForHomePage className={classes.service} />
+          <ServiceCardForHomePage />
+          <ServiceCardForHomePage />
+          <ServiceCardForHomePage />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
---
+++ client/src/pages/Home/style.js
@@ -0,0 +1,57 @@
+import { makeStyles } from '@material-ui/core';
+import landingBackground from '../../assets/images/landingBackground.jpg';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    alignItems: 'justify-content',
+  },
+  firstSec: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    backgroundImage: `url(${landingBackground})`,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '100% 100%',
+    height: '100vh',
+  },
+  intro: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    width: '50%',
+    marginBottom: '2rem',
+    paddingRight: '5rem',
+  },
+  text: {
+    marginBottom: '1rem',
+  },
+  text2: {
+    marginBottom: '2.5rem',
+  },
+  button: {
+    width: '25%',
+    marginBottom: '0.7rem',
+    alignSelf: 'center',
+  },
+  buyNowBtn: {
+    background: theme.palette.primary.main,
+    color: '#fff',
+  },
+  servicesSec: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: '1rem',
+  },
+  services: {
+    display: 'flex',
+    flexDirection: 'row',
+    marginTop: '2rem',
+  },
+  service: {
+    paddingRight: '1rem',
+  },
+}));
+
+export default useStyles;

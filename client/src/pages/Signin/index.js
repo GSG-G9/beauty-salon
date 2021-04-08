c47248259a7eac5e5562291b7a047d6ef7e6ae60
yasmeenattallah <yattallah7@gmail.com>
2021-04-08 23:04:46 +0300

    add page structure and style Relates #14

--- client/src/pages/Signin/index.js
+++ client/src/pages/Signin/index.js
@@ -1,21 +1,76 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Container from '@material-ui/core/Container';
-import { Typography } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Avatar from '@material-ui/core/Avatar';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 import { InputField, ButtonComponent } from '../../component';
+import { SIGNUP_PAGE } from '../../utils/router.constant';
 import useStyles from './style';
 
 const Signin = () => {
   const classes = useStyles();
   return (
-    <Container className={classes.root}>
-      <form className={classes.form}>
-        <InputField label="Email" />
-        <InputField label="password" />
-        <ButtonComponent> Login </ButtonComponent>
-        <Typography color="primary">
-          Don&#39;t have an account ? sign up
-        </Typography>
-      </form>
+    <Container>
+      <Grid container component="main" className={classes.root}>
+        <Grid item xs={false} sm={4} md={7} className={classes.image} />
+        <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+          <div className={classes.paper}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
+            <form className={classes.form} noValidate>
+              <InputField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+              />
+              <InputField
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+              <ButtonComponent
+                type="submit"
+                fullWidth
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign In
+              </ButtonComponent>
+              <Grid container>
+                <Grid item>
+                  <Typography color="primary">
+                    Don&#39;t have an account ?
+                    <Link to={SIGNUP_PAGE} className={classes.link}>
+                      sign up
+                    </Link>
+                  </Typography>
+                </Grid>
+              </Grid>
+            </form>
+          </div>
+        </Grid>
+      </Grid>
     </Container>
   );
 };
--- client/src/pages/Signin/style.js
+++ client/src/pages/Signin/style.js
@@ -1,5 +1,49 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(() => {});
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor: theme.palette.primary.type,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(4, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    fontSize: '1.2rem',
+    color: '#fff',
+  },
+  container: {
+    display: 'flex',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginTop: 50,
+  },
+  iconLock: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: theme.spacing(0, 7, 1, 0),
+  },
+  link: {
+    color: theme.palette.primary.main,
+  },
+}));
 
 export default useStyles;

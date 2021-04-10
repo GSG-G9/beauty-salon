8f38fdbcc95b34e9d726989aa54c578a996d3c70
Khamis Hesham <khamis1997.kh@gmail.com>
2021-04-10 12:56:59 +0300

    create signup page

--- client/src/pages/Signup/index.js
+++ client/src/pages/Signup/index.js
@@ -1,5 +1,218 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { useHistory, Link } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Avatar from '@material-ui/core/Avatar';
+import axios from 'axios';
+import { SIGNIN_PAGE } from '../../utils/router.constant';
+import { InputField } from '../../component';
+import useStyles from './style';
+import {
+  updateAndValidateInput,
+  firstNameSchema,
+  lastNameSchema,
+  emailSchema,
+  passwordSchema,
+  // confirmPasswordSchema,
+} from '../../utils';
 
-const Signup = () => <div>Hi from Signup</div>;
+const Signup = () => {
+  const classes = useStyles();
+  const history = useHistory();
+
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  // const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [firstNameError, setFirstNameError] = useState(false);
+  const [lastNameError, setLastNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  // const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+
+  const [, setLoading] = useState(false);
+  const [, setError] = useState('');
+
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      setLoading(true);
+      // if (password !== confirmPassword) {
+      //   return setConfirmPasswordError('Please input your the same password!');
+      // }
+      // || !confirmPassword
+      if (!email || !password) {
+        return setLoading(false);
+      }
+
+      await axios.post('/api/v1/signup', {
+        firstName,
+        lastName,
+        email,
+        password,
+        // confirmPassword,
+      });
+      setLoading(false);
+      setError('');
+      return history.push('/signin');
+    } catch (err) {
+      setLoading(false);
+      return setError(err.response.data.message || 'Something went wrong !! ');
+    }
+  };
+
+  return (
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5}>
+        <div className={classes.paper}>
+          <div className={classes.iconLock}>
+            <Avatar className={classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+          </div>
+          <form className={classes.form}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <InputField
+                  required
+                  fullWidth
+                  id="firstName"
+                  name="firstName"
+                  label="First Name"
+                  value={firstName}
+                  onChange={updateAndValidateInput(
+                    'firstName',
+                    firstNameSchema,
+                    setFirstName,
+                    setFirstNameError
+                  )}
+                  error={firstNameError}
+                  helperText={
+                    firstNameError ? 'Please Enter A valid First Name' : null
+                  }
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <InputField
+                  required
+                  fullWidth
+                  id="lastName"
+                  name="lastName"
+                  label="Last Name"
+                  value={lastName}
+                  onChange={updateAndValidateInput(
+                    'lastName',
+                    lastNameSchema,
+                    setLastName,
+                    setLastNameError
+                  )}
+                  error={lastNameError}
+                  helperText={
+                    lastNameError ? 'Please Enter A valid Last Name' : null
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <InputField
+                  required
+                  fullWidth
+                  id="email"
+                  name="email"
+                  type="email"
+                  label="Email Address"
+                  value={email}
+                  onChange={updateAndValidateInput(
+                    'email',
+                    emailSchema,
+                    setEmail,
+                    setEmailError
+                  )}
+                  error={emailError}
+                  helperText={
+                    emailError
+                      ? 'Please Enter A valid Email like exmaple@test.com'
+                      : null
+                  }
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <InputField
+                  required
+                  fullWidth
+                  id="password"
+                  name="password"
+                  type="password"
+                  label="Password"
+                  value={password}
+                  onChange={updateAndValidateInput(
+                    'password',
+                    passwordSchema,
+                    setPassword,
+                    setPasswordError
+                  )}
+                  error={passwordError}
+                  helperText={
+                    passwordError
+                      ? 'Please Enter A valid Password which must contain 6 characters'
+                      : null
+                  }
+                />
+              </Grid>
+              {/* <Grid item xs={12}>
+                <InputField
+                  required
+                  fullWidth
+                  id="confirmPassword"
+                  type="password"
+                  label="Confirm Password"
+                  value={confirmPassword}
+                  onChange={updateAndValidateInput(
+                    'confirmPassword',
+                    confirmPasswordSchema,
+                    setConfirmPassword,
+                    setConfirmPasswordError
+                  )}
+                  error={confirmPasswordError}
+                  helperText={
+                    confirmPasswordError
+                      ? 'Please Enter A valid Password which must contain 6 characters'
+                      : null
+                  }
+                />
+              </Grid> */}
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={handleSubmit}
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="flex-start">
+              <Grid item>
+                <Link to={SIGNIN_PAGE} className={classes.link}>
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
+        </div>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Signup;
---
+++ client/src/pages/Signup/style.js
@@ -0,0 +1,51 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: '100vh',
+  },
+  image: {
+    backgroundImage: 'url(https://source.unsplash.com/random)',
+    backgroundRepeat: 'no-repeat',
+    backgroundColor:
+      theme.palette.type === 'light'
+        ? theme.palette.grey[50]
+        : theme.palette.grey[900],
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+  paper: {
+    margin: theme.spacing(8, 4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    fontSize: '1.2rem',
+  },
+  // container: {
+  //   display: 'flex',
+  //   justifyContent: 'space-around',
+  //   width: '100%',
+  //   marginTop: 50,
+  // },
+  iconLock: {
+    display: 'flex',
+    alignItems: 'center',
+    margin: theme.spacing(0, 7, 1, 0),
+  },
+  link: {
+    color: theme.palette.primary.main,
+  },
+}));
+
+export default useStyles;

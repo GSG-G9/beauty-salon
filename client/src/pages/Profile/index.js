a71c06fdbcbdf4de0b33db92eaa15b61497673c1
Khamis Hesham <khamis1997.kh@gmail.com>
2021-04-12 00:19:08 +0300

    get and displaying user information

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Typography, Paper } from '@material-ui/core';
+import axios from 'axios';
+import useStyles from './style';
 
-const Profile = () => <div>Hi from Profile</div>;
+const UserInfo = () => {
+  const classes = useStyles();
+  const [userData, setUserData] = useState([]);
+  const [, setError] = useState('');
+  useEffect(() => {
+    const { CancelToken } = axios;
+    const source = CancelToken.source();
+    (async () => {
+      try {
+        const {
+          data: { data },
+        } = await axios.get(`/api/v1/profile`);
+        setUserData(data);
+      } catch (err) {
+        setError(err.response ? err.response.data.message : err.errors[0]);
+      }
+    })();
+    return () => source.cancel('Operation canceled');
+  }, []);
 
-export default Profile;
+  return (
+    <Paper className={classes.userData}>
+      <div className={classes.userDataField}>
+        <Typography className={classes.userDataFieldLabel}>
+          Username:{' '}
+        </Typography>
+        <Typography className={classes.userDataFieldValue}>
+          {userData.first_name} {userData.last_name}
+        </Typography>
+      </div>
+      <div className={classes.userDataField}>
+        <Typography className={classes.userDataFieldLabel}>Email: </Typography>
+        <Typography className={classes.userDataFieldValue}>
+          {userData.email}
+        </Typography>
+      </div>
+      <div className={classes.userDataField}>
+        <Typography className={classes.userDataFieldLabel}>Mobile: </Typography>
+        <Typography className={classes.userDataFieldValue}>
+          {userData.mobile ? userData.mobile : '--'}
+        </Typography>
+      </div>
+      <div className={classes.userDataField}>
+        <Typography className={classes.userDataFieldLabel}>
+          Address:{' '}
+        </Typography>
+        <Typography className={classes.userDataFieldValue}>
+          {userData.address ? userData.address : '--'}
+        </Typography>
+      </div>
+    </Paper>
+  );
+};
+
+export default UserInfo;
---
+++ client/src/pages/Profile/style.js
@@ -0,0 +1,26 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  userData: {
+    fontColor: theme.palette.secondary.main,
+    marginTop: 60,
+    backgroundColor: theme.palette.background.default,
+  },
+  userDataField: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    paddingBlock: '0.5em',
+    paddingLeft: '3em',
+    borderBottom: '1px solid #c7c7c7',
+    justifyContent: 'center',
+  },
+  userDataFieldLabel: {
+    width: '12%',
+  },
+  userDataFieldValue: {
+    width: '50%',
+  },
+}));
+
+export default useStyles;

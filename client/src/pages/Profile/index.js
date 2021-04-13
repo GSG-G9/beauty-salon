b870b8e7e8d9bdac61d83464a4921bfd35283c22
Khamis Hesham <khamis1997.kh@gmail.com>
2021-04-14 01:27:41 +0300

    fix style

--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -1,99 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { Typography, Paper, Avatar, Grid } from '@material-ui/core';
-import axios from 'axios';
+import React from 'react';
+
+import { Container } from '@material-ui/core';
+
+import SideBar from './SideBar';
 import useStyles from './style';
-import UpdateUser from './UpdateInfo';
 
-const UserInfo = () => {
+function Profile() {
   const classes = useStyles();
-  const [userData, setUserData] = useState([]);
-  const [, setError] = useState('');
-
-  // const [loading, setLoading] = useState(false);
-  // const [errorMsg, setErrorMsg] = useState('');
-  const [isUpdate, setIsUpdate] = useState(false);
-  const [, setOpen] = useState(false);
-
-  const handleClose = (event, reason) => {
-    if (reason === 'clickaway') {
-      return;
-    }
-    setOpen(false);
-  };
-  const handleClick = () => {
-    setOpen(true);
-  };
-
-  useEffect(() => {
-    const { CancelToken } = axios;
-    const source = CancelToken.source();
-    (async () => {
-      try {
-        const {
-          data: { data },
-        } = await axios.get(`/api/v1/profile`);
-        setUserData(data);
-      } catch (err) {
-        setError(err.response ? err.response.data.message : err.errors[0]);
-      }
-    })();
-    return () => source.cancel('Operation canceled');
-  }, [isUpdate]);
 
   return (
-    <>
-      <Paper className={classes.userData}>
-        <Typography variant="h5">User Information</Typography>
-        <Grid>
-          <Avatar className={classes.avatar} />
-        </Grid>
-        <div className={classes.userDataField}>
-          <Typography className={classes.userDataFieldLabel}>
-            Username:
-          </Typography>
-          <Typography className={classes.userDataFieldValue}>
-            {userData.first_name} {userData.last_name}
-          </Typography>
-        </div>
-        <div className={classes.userDataField}>
-          <Typography className={classes.userDataFieldLabel}>
-            Email:{' '}
-          </Typography>
-          <Typography className={classes.userDataFieldValue}>
-            {userData.email}
-          </Typography>
-        </div>
-        <div className={classes.userDataField}>
-          <Typography className={classes.userDataFieldLabel}>
-            Mobile:{' '}
-          </Typography>
-          <Typography className={classes.userDataFieldValue}>
-            {userData.mobile ? userData.mobile : '--'}
-          </Typography>
-        </div>
-        <div className={classes.userDataField}>
-          <Typography className={classes.userDataFieldLabel}>
-            Address:
-          </Typography>
-          <Typography className={classes.userDataFieldValue}>
-            {userData.address ? userData.address : '--'}
-          </Typography>
-        </div>
-      </Paper>
-      <div className={classes.btnsBox}>
-        <UpdateUser
-          handleClickAlert={handleClick}
-          handleCloseAlert={handleClose}
-          setUpdateUser={setIsUpdate}
-          userData={{
-            username: userData.first_name,
-            address: userData.address,
-            mobile: userData.mobile,
-          }}
-        />
-      </div>
-    </>
+    <Container maxWidth="lg" className={classes.root}>
+      <SideBar />
+    </Container>
   );
-};
+}
 
-export default UserInfo;
+export default Profile;
--- client/src/pages/Profile/style.js
+++ client/src/pages/Profile/style.js
@@ -1,29 +1,9 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  avatar: {
-    width: '5em',
-    height: '5em',
-  },
-  userData: {
-    fontColor: theme.palette.secondary.main,
-    marginTop: 60,
-    backgroundColor: theme.palette.background.default,
-  },
-  userDataField: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'row',
-    paddingBlock: '0.5em',
-    paddingLeft: '3em',
-    borderBottom: '1px solid #c7c7c7',
-    justifyContent: 'center',
-  },
-  userDataFieldLabel: {
-    width: '12%',
-  },
-  userDataFieldValue: {
-    width: '50%',
+const useStyles = makeStyles(() => ({
+  root: {
+    width: '85%',
+    padding: '3em',
   },
 }));
 

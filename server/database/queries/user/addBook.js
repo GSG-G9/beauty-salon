1bbb9b4b14078c82b5ef328a3993716517c1facd
imansedky5 <imans.ewaiti@gmail.com>
2021-04-14 02:48:26 +0300

    edit addBook controller and addBookingValidation

--- server/database/queries/user/addBook.js
+++ server/database/queries/user/addBook.js
@@ -1,16 +1,12 @@
 const connection = require('../../config/connection');
 
 const addBook = ({
-  userId,
-  serviceId,
-  stylistId,
-  appointmentDate,
-  appointmentTime,
+  userId, serviceId, appointmentDate, appointmentTime,
 }) => {
   const sql = {
     text:
-      'INSERT INTO appointments (user_id, service_id, stylist_id, appointment_date, appointment_time) VALUES ($1,$2,$3,$4,$5) RETURNING appointment_date, appointment_time;',
-    values: [userId, serviceId, stylistId, appointmentDate, appointmentTime],
+      'INSERT INTO appointments (user_id, service_id, appointment_date, appointment_time) VALUES ($1,$2,$3,$4) RETURNING appointment_date, appointment_time;',
+    values: [userId, serviceId, appointmentDate, appointmentTime],
   };
   return connection.query(sql);
 };

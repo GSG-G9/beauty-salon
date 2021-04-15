9d3ec045a9bff317da6c7f889627d4ce8cbcf140
Khamis Hesham <khamis1997.kh@gmail.com>
2021-04-15 13:05:17 +0300

    fix user info file

--- client/src/pages/Profile/UserInfo/index.js
+++ client/src/pages/Profile/UserInfo/index.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Typography, Paper, Avatar, Grid } from '@material-ui/core';
+import { Typography, Paper, Grid } from '@material-ui/core';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import Alert from '@material-ui/lab/Alert';
 import useStyles from './style';
 import UpdateUser from '../UpdateInfo';
 import { Loading } from '../../../component';
 
-const UserInfo = () => {
+const UserInfo = ({ getUserName }) => {
   const classes = useStyles();
   const [userData, setUserData] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -14,7 +15,7 @@ const UserInfo = () => {
   const [isUpdate, setIsUpdate] = useState(false);
   const [, setOpen] = useState(false);
 
-  const handleClose = (event, reason) => {
+  const handleClose = (reason) => {
     if (reason === 'clickaway') {
       return;
     }
@@ -34,6 +35,7 @@ const UserInfo = () => {
           data: { data },
         } = await axios.get(`/api/v1/profile`);
         setUserData(data);
+        getUserName(data.first_name);
         setLoading(false);
       } catch (err) {
         setLoading(false);
@@ -41,59 +43,64 @@ const UserInfo = () => {
       }
     })();
     return () => source.cancel('Operation canceled');
-  }, [isUpdate]);
+  }, [isUpdate, getUserName]);
 
   return (
     <div className={classes.root}>
-      <Typography variant="h5">User Information</Typography>
+      <Typography variant="h2" className={classes.title}>
+        User Information
+      </Typography>
       {loading ? (
         <Loading />
       ) : (
-        <>
-          <Grid>
-            <Avatar className={classes.avatar} />
-          </Grid>
-          <Grid justify="center">
-            {error ? (
-              <Alert variant="error">{error}</Alert>
-            ) : (
-              <Paper className={classes.userData}>
-                <div className={classes.userDataField}>
-                  <Typography className={classes.userDataFieldLabel}>
-                    Username:
-                  </Typography>
-                  <Typography className={classes.userDataFieldValue}>
-                    {userData.first_name} {userData.last_name}
-                  </Typography>
-                </div>
-                <div className={classes.userDataField}>
-                  <Typography className={classes.userDataFieldLabel}>
-                    Email:
-                  </Typography>
-                  <Typography className={classes.userDataFieldValue}>
-                    {userData.email}
-                  </Typography>
-                </div>
-                <div className={classes.userDataField}>
-                  <Typography className={classes.userDataFieldLabel}>
-                    Mobile:
-                  </Typography>
-                  <Typography className={classes.userDataFieldValue}>
-                    {userData.mobile ? userData.mobile : '--'}
-                  </Typography>
-                </div>
-                <div className={classes.userDataField}>
-                  <Typography className={classes.userDataFieldLabel}>
-                    Address:
-                  </Typography>
-                  <Typography className={classes.userDataFieldValue}>
-                    {userData.address ? userData.address : '--'}
-                  </Typography>
-                </div>
-              </Paper>
-            )}
-          </Grid>
-        </>
+        <Grid>
+          {error ? (
+            <Alert variant="error">{error}</Alert>
+          ) : (
+            <Paper className={classes.userData}>
+              <div className={classes.userDataField}>
+                <Typography className={classes.userDataFieldLabel}>
+                  First Name:
+                </Typography>
+                <Typography className={classes.userDataFieldValue}>
+                  {userData.first_name}
+                </Typography>
+              </div>
+              <div className={classes.userDataField}>
+                <Typography className={classes.userDataFieldLabel}>
+                  last Name:
+                </Typography>
+                <Typography className={classes.userDataFieldValue}>
+                  {userData.last_name}
+                </Typography>
+              </div>
+              <div className={classes.userDataField}>
+                <Typography className={classes.userDataFieldLabel}>
+                  Email:
+                </Typography>
+                <Typography className={classes.userDataFieldValue}>
+                  {userData.email}
+                </Typography>
+              </div>
+              <div className={classes.userDataField}>
+                <Typography className={classes.userDataFieldLabel}>
+                  Mobile:
+                </Typography>
+                <Typography className={classes.userDataFieldValue}>
+                  {userData.mobile ? userData.mobile : '--'}
+                </Typography>
+              </div>
+              <div className={classes.userDataField}>
+                <Typography className={classes.userDataFieldLabel}>
+                  Address:
+                </Typography>
+                <Typography className={classes.userDataFieldValue}>
+                  {userData.address ? userData.address : '--'}
+                </Typography>
+              </div>
+            </Paper>
+          )}
+        </Grid>
       )}
 
       <div className={classes.btnsBox}>
@@ -111,6 +118,10 @@ const UserInfo = () => {
       </div>
     </div>
   );
+};
+const { func } = PropTypes;
+UserInfo.propTypes = {
+  getUserName: func.isRequired,
 };
 
 export default UserInfo;

cfa532d7971d9b842a0d83b1e56931778831f8bd
Khamis Hesham <khamis1997.kh@gmail.com>
2021-04-16 00:41:45 +0300

    make reservations mobile responsive

--- client/src/pages/Profile/Reservations/style.js
+++ client/src/pages/Profile/Reservations/style.js
@@ -2,6 +2,9 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    '@media (max-device-width: 600px)': {
+      width: '19rem',
+    },
     display: 'flex',
     justifyContent: 'flex-start',
     flexDirection: 'column',
@@ -17,6 +20,9 @@ const useStyles = makeStyles((theme) => ({
       marginBottom: '7%',
       fontWeight: 'bold',
       color: theme.palette.primary.main,
+      '@media (max-device-width: 600px)': {
+        fontSize: '2em',
+      },
     },
     '& h3': {
       fontSize: '2.2em',
@@ -26,6 +32,11 @@ const useStyles = makeStyles((theme) => ({
       marginBottom: '7%',
       fontWeight: 'bold',
       color: theme.palette.primary.main,
+      '@media (max-device-width: 600px)': {
+        fontSize: '1.8em',
+        marginTop: '35%',
+        marginLeft: '7%',
+      },
     },
   },
 }));

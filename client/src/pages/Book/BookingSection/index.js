939a5011f08a4a9aa02424e6ee877c306d72f48b
imansedky5 <imans.ewaiti@gmail.com>
2021-04-17 02:23:31 +0300

    filter available times before display in the select input

--- client/src/pages/Book/BookingSection/index.js
+++ client/src/pages/Book/BookingSection/index.js
@@ -1,9 +1,7 @@
 import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
-
-import { Button, Grid } from '@material-ui/core';
-
-import { InputDate, InputTime, SelectInput } from '../../../component';
+import { Button, Grid, Typography } from '@material-ui/core';
+import { InputDate, SelectInput } from '../../../component';
 import { userContext } from '../../../utils';
 import useStyles from './style';
 
@@ -13,16 +11,17 @@ const BookingSection = () => {
   const [selectedServiceId, setSelectedServiceId] = useState('');
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [selectedTime, setSelectedTime] = useState(new Date());
-  const [time, setConvertedTime] = useState(new Date());
   const [convertedDate, setConvertedDate] = useState(new Date());
   const [catagoryValue, setCatagoryValue] = useState('');
   const [services, setServices] = useState([]);
+  const [freeTimes, setFreeTimes] = useState([]);
   const [role, userData] = useContext(userContext);
-  const [dateErrorMsg, setDateErrorMsg] = useState('');
+  const [chosenTimeErrorMsg, setChosenTimeErrorMsg] = useState('');
   const [isClicked, setIsClicked] = useState(false);
   const [bookingMessage, setBookingMessage] = useState('');
   const [catagoryErrorMsg, setCatagoryErrorMsg] = useState('');
   const [serviceErrorMsg, setServiceErrorMsg] = useState('');
+  const [emptyTimeFieldErrorMsg, setEmptyTimeFieldErrorMsg] = useState('');
 
   const userId = userData.id;
   // eslint-disable-next-line no-console
@@ -42,21 +41,21 @@ const BookingSection = () => {
     setConvertedDate(date);
   };
 
-  const handleTimeChange = (date) => {
-    // eslint-disable-next-line no-unused-vars
-    const timePicker = date.toString().substr(16, 5);
-    setSelectedTime(date);
-    setConvertedTime(timePicker);
+  const handleTimeChange = (e) => {
+    setSelectedTime(e.target.value);
   };
   const handelSentData = () => {
     if (!catagoryValue) {
       setCatagoryErrorMsg('please select catagory');
     } else if (!selectedServiceId) {
       setServiceErrorMsg('please select service');
+    } else if (!selectedTime) {
+      setEmptyTimeFieldErrorMsg('please select time');
     } else {
       setIsClicked(true);
       setCatagoryErrorMsg('');
       setServiceErrorMsg('');
+      setEmptyTimeFieldErrorMsg('');
     }
   };
 
@@ -71,7 +70,6 @@ const BookingSection = () => {
         } = await axios.get('/api/v1/services', {
           CancelToken: source.token,
         });
-        setServices(data);
         const servicesFilteredByCatagory = data
           .filter((el) => el.category === catagoryValue)
           .map(({ name, id }) => ({ name, val: id.toString() }));
@@ -99,63 +97,127 @@ const BookingSection = () => {
               userId,
               serviceId: Number(selectedServiceId),
               appointmentDate: convertedDate.toISOString().substr(0, 10),
-              appointmentTime: time,
+              appointmentTime: selectedTime,
             },
             { CancelToken: source.token }
           );
-          console.log(message);
           setBookingMessage(message);
-          setDateErrorMsg('');
+          setChosenTimeErrorMsg('');
           setIsClicked(false);
         } catch (err) {
           setBookingMessage('');
-          setDateErrorMsg('chosen date in not available, select another date');
+          setChosenTimeErrorMsg(
+            'chosen time in not available, select another time'
+          );
           setIsClicked(false);
         }
       })();
     }
 
     return () => source.cancel();
-  }, [selectedServiceId, convertedDate, time, userId, isClicked]);
+  }, [selectedServiceId, convertedDate, selectedTime, userId, isClicked]);
+
+  useEffect(() => {
+    const { CancelToken } = axios;
+    const source = CancelToken.source();
+
+    (async () => {
+      try {
+        const {
+          data: { data },
+        } = await axios.get(`/api/v1/bookings/${selectedDate}`, {
+          CancelToken: source.token,
+        });
+        const busyTimes = data.map((el) => el.appointment_time);
+        const workTimes = [
+          '10:00',
+          '11:00',
+          '12:00',
+          '13:00',
+          '14:00',
+          '15:00',
+          '16:00',
+        ];
+        const newArr = busyTimes.concat(workTimes);
+        const freeTimesInCertainDate = newArr.filter(
+          (el) => newArr.indexOf(el) === newArr.lastIndexOf(el)
+        );
+
+        setFreeTimes(
+          freeTimesInCertainDate.map((el) => ({
+            name: el,
+            val: el,
+          }))
+        );
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      }
+    })();
+    return () => source.cancel();
+  }, [selectedDate]);
 
   return (
-    <>
-      <Grid container className={classes.bookingSectionContainer}>
-        <Grid container className={classes.dateTimeContainer}>
-          <InputDate onChange={handleDateChange} selectedDate={selectedDate} />
-          {dateErrorMsg && <div> {dateErrorMsg}</div>}
-          <InputTime onChange={handleTimeChange} selectedTime={selectedTime} />
-        </Grid>
-        <Grid container className={classes.selectInputsContainer}>
-          <SelectInput
-            handleChange={handleChange}
-            value={catagoryValue}
-            label="Select Catagory"
-            menu={[
-              { name: 'Hair', val: 'Hair cuts' },
-              { name: 'Makeup', val: 'Make up' },
-              { name: 'Nails', val: 'nails' },
-              { name: 'Skin Care', val: 'Skin care' },
-            ]}
-          />
-          {catagoryErrorMsg && <div>{catagoryErrorMsg}</div>}
+    <Grid container className={classes.bookingSectionContainer}>
+      <InputDate onChange={handleDateChange} selectedDate={selectedDate} />
+      {freeTimes && (
+        <SelectInput
+          handleChange={handleTimeChange}
+          value={selectedTime}
+          label="Select Time"
+          menu={freeTimes}
+        />
+      )}
+      {emptyTimeFieldErrorMsg && (
+        <Typography className={classes.errorMessage}>
+          {emptyTimeFieldErrorMsg}
+        </Typography>
+      )}
 
-          {services && (
-            <SelectInput
-              handleChange={handleServiceChange}
-              value={selectedServiceId}
-              label="Select Service"
-              menu={services}
-            />
-          )}
-          {serviceErrorMsg && <div>{serviceErrorMsg}</div>}
-        </Grid>
-      </Grid>
-      <Button className={classes.sendBtn} onClick={handelSentData}>
+      <SelectInput
+        handleChange={handleChange}
+        value={catagoryValue}
+        label="Select Catagory"
+        menu={[
+          { name: 'Hair', val: 'Hair cuts' },
+          { name: 'Makeup', val: 'Make up' },
+          { name: 'Nails', val: 'nails' },
+          { name: 'Skin Care', val: 'Skin care' },
+        ]}
+      />
+      {catagoryErrorMsg && (
+        <Typography className={classes.errorMessage}>
+          {catagoryErrorMsg}
+        </Typography>
+      )}
+      {services && (
+        <SelectInput
+          handleChange={handleServiceChange}
+          value={selectedServiceId}
+          label="Select Service"
+          menu={services}
+        />
+      )}
+      {serviceErrorMsg && (
+        <Typography className={classes.errorMessage}>
+          {serviceErrorMsg}
+        </Typography>
+      )}
+
+      <Button className={classes.submitBtn} onClick={handelSentData}>
         SEND
       </Button>
-      {bookingMessage && <div>{bookingMessage}</div>}
-    </>
+      {bookingMessage && (
+        <Typography className={classes.successfulMessage}>
+          {bookingMessage}
+        </Typography>
+      )}
+      {chosenTimeErrorMsg && (
+        <Typography className={classes.errorMessage}>
+          {chosenTimeErrorMsg}
+        </Typography>
+      )}
+    </Grid>
   );
 };
 
--- server/utilis/validation/addBookingValidation.js
+++ server/utilis/validation/addBookingValidation.js
@@ -5,7 +5,7 @@ const addBookingValidation = Joi.object({
   serviceId: Joi.number().required(),
   appointmentDate: Joi.string().required(),
   // eslint-disable-next-line no-useless-escape
-  appointmentTime: Joi.string().required(),
+  appointmentTime: Joi.string().regex(/^([0-9]{2}):([0]{2})$/).required(),
 });
 
 module.exports = addBookingValidation;

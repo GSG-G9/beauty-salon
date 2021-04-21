7a67dc473ae0a0de05d1e755955f8b12fe916c6b
imansedky5 <imans.ewaiti@gmail.com>
2021-04-21 11:59:08 +0300

    solve conflict

--- client/src/pages/Book/BookingSection/index.js
+++ client/src/pages/Book/BookingSection/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState, useContext } from 'react';
 import axios from 'axios';
-import { Button, Grid, Typography } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import Snackbar from '@material-ui/core/Snackbar';
 import Alert from '@material-ui/lab/Alert';
 import { InputDate, SelectInput } from '../../../component';
@@ -9,6 +9,7 @@ import useStyles from './style';
 
 const BookingSection = () => {
   const classes = useStyles();
+
   const [selectedServiceId, setSelectedServiceId] = useState('');
   const [selectedDate, setSelectedDate] = useState(
     new Date().toISOString().substr(0, 10)
@@ -19,12 +20,17 @@ const BookingSection = () => {
   const [services, setServices] = useState([]);
   const [freeTimes, setFreeTimes] = useState([]);
   const [, , userData] = useContext(userContext);
-  const [chosenTimeErrorMsg, setChosenTimeErrorMsg] = useState('');
   const [isClicked, setIsClicked] = useState(false);
+  const [chosenTimeErrorMsg, setChosenTimeErrorMsg] = useState('');
+  const [openTimeAlert, setOpenTimeAlert] = useState(false);
   const [bookingMessage, setBookingMessage] = useState('');
+  const [openBookingAlert, setOpenBookingAlert] = useState(false);
   const [categoryErrorMsg, setCategoryErrorMsg] = useState('');
+  const [openCategoryAlert, setOpenCategoryAlert] = useState(false);
   const [serviceErrorMsg, setServiceErrorMsg] = useState('');
+  const [openServiceAlert, setOpenServiceAlert] = useState(false);
   const [emptyTimeFieldErrorMsg, setEmptyTimeFieldErrorMsg] = useState('');
+  const [openEmptyTimeAlert, setOpenEmptyTimeAlert] = useState(false);
 
   const userId = userData.id;
 
@@ -43,6 +49,46 @@ const BookingSection = () => {
     setConvertedDate(date);
   };
 
+  const handleCloseEmptyTimeAlert = (event, reason) => {
+    if (reason === 'clickaway') {
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
+    setOpenEmptyTimeAlert(false);
+  };
+  const handleCloseCategoryAlert = (event, reason) => {
+    if (reason === 'clickaway') {
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
+    setOpenCategoryAlert(false);
+  };
+
+  const handleCloseServiceAlert = (event, reason) => {
+    if (reason === 'clickaway') {
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
+    setOpenServiceAlert(false);
+  };
+
+  const handleCloseBookingMessage = (event, reason) => {
+    if (reason === 'clickaway') {
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
+    setOpenBookingAlert(false);
+  };
+
+  const handleCloseTimeErrAlert = (event, reason) => {
+    if (reason === 'clickaway') {
+      // eslint-disable-next-line no-useless-return
+      return;
+    }
+
+    setOpenTimeAlert(false);
+  };
+
   const handleTimeChange = (e) => {
     setSelectedTime(e.target.value);
   };
@@ -50,10 +96,13 @@ const BookingSection = () => {
   const handelSentData = () => {
     if (!categoryValue) {
       setCategoryErrorMsg('please select category');
+      setOpenCategoryAlert(true);
     } else if (!selectedServiceId) {
       setServiceErrorMsg('please select service');
+      setOpenServiceAlert(true);
     } else if (!selectedTime) {
       setEmptyTimeFieldErrorMsg('please select time');
+      setOpenEmptyTimeAlert(true);
     } else {
       setIsClicked(true);
       setCategoryErrorMsg('');
@@ -105,12 +154,14 @@ const BookingSection = () => {
           );
           setBookingMessage(message);
           setChosenTimeErrorMsg('');
+          setOpenBookingAlert(true);
           setIsClicked(false);
         } catch (err) {
           setBookingMessage('');
           setChosenTimeErrorMsg(
             'chosen time in not available, select another time'
           );
+          setOpenTimeAlert(true);
           setIsClicked(false);
         }
       })();
@@ -173,11 +224,16 @@ const BookingSection = () => {
         />
       )}
       {emptyTimeFieldErrorMsg && (
-        <Typography className={classes.errorMessage} color="secondary">
-          {emptyTimeFieldErrorMsg}
-        </Typography>
+        <Snackbar
+          open={openEmptyTimeAlert}
+          autoHideDuration={3000}
+          onClose={handleCloseEmptyTimeAlert}
+        >
+          <Alert severity="warning" onClose={handleCloseEmptyTimeAlert}>
+            {emptyTimeFieldErrorMsg}
+          </Alert>
+        </Snackbar>
       )}
-
       <SelectInput
         handleChange={handleChange}
         value={categoryValue}
@@ -190,9 +246,15 @@ const BookingSection = () => {
         ]}
       />
       {categoryErrorMsg && (
-        <Typography className={classes.errorMessage}>
-          {categoryErrorMsg}
-        </Typography>
+        <Snackbar
+          open={openCategoryAlert}
+          autoHideDuration={3000}
+          onClose={handleCloseCategoryAlert}
+        >
+          <Alert severity="warning" onClose={handleCloseCategoryAlert}>
+            {categoryErrorMsg}
+          </Alert>
+        </Snackbar>
       )}
       {services && (
         <SelectInput
@@ -203,9 +265,15 @@ const BookingSection = () => {
         />
       )}
       {serviceErrorMsg && (
-        <Typography className={classes.errorMessage}>
-          {serviceErrorMsg}
-        </Typography>
+        <Snackbar
+          open={openServiceAlert}
+          autoHideDuration={3000}
+          onClose={handleCloseServiceAlert}
+        >
+          <Alert severity="warning" onClose={handleCloseServiceAlert}>
+            {serviceErrorMsg}
+          </Alert>
+        </Snackbar>
       )}
       <Button
         className={classes.submitBtn}
@@ -215,13 +283,25 @@ const BookingSection = () => {
         SEND
       </Button>
       {bookingMessage && (
-        <Snackbar open autoHideDuration={8000}>
-          <Alert severity="success">{bookingMessage}</Alert>
+        <Snackbar
+          open={openBookingAlert}
+          autoHideDuration={3000}
+          onClose={handleCloseBookingMessage}
+        >
+          <Alert severity="success" onClose={handleCloseBookingMessage}>
+            {bookingMessage}
+          </Alert>
         </Snackbar>
       )}
       {chosenTimeErrorMsg && (
-        <Snackbar open autoHideDuration={8000}>
-          <Alert severity="error">{chosenTimeErrorMsg}</Alert>
+        <Snackbar
+          open={openTimeAlert}
+          autoHideDuration={3000}
+          onClose={handleCloseTimeErrAlert}
+        >
+          <Alert severity="error" onClose={handleCloseTimeErrAlert}>
+            {chosenTimeErrorMsg}
+          </Alert>
         </Snackbar>
       )}
     </Grid>
